d7bdda8db4f6c352314f695bd8cc4aee020d6670
unknown <manas842500@gmail.com>
2025-03-01 12:58:05 +0530

    login error fixing

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -15,9 +15,11 @@ const Login = () => {
 
     const handleLogin = async () => {
         try {
-            let response = await fetch('http://localhost:5000/login', {
+            let response = await fetch('https://warehousestore-1.onrender.com/login', {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
+                headers: {
+                    'Content-Type': 'application/json',
+                },
                 body: JSON.stringify({ email, password }),
             });
 
