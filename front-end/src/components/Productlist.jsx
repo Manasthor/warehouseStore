185071ddcdb28b41a64302e5ce7ba4a3bc26e2ea
unknown <manas842500@gmail.com>
2025-03-02 12:32:09 +0530

    fixing

--- front-end/src/components/Productlist.jsx
+++ front-end/src/components/Productlist.jsx
@@ -67,12 +67,12 @@ const Productlist = () => {
                 />
             </div>
             <ul className="flex justify-around items-center p-2 border border-blue-400 bg-blue-100 rounded-xl shadow-sm text-sm font-medium text-gray-700 tracking-wide w-full max-w-full mx-auto">
-                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">S No.</li>
-                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">Name</li>
-                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">Price</li>
-                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">Category</li>
-                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">Company</li>
-                <li className="px-2 py-2 text-center flex-1">Action</li>
+                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300 font-extrabold">S No.</li>
+                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300 font-extrabold">Name</li>
+                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300 font-extrabold">Price</li>
+                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300 font-extrabold">Category</li>
+                <li className="px-2 py-2 text-center flex-1 border-r border-gray-300 font-extrabold">Company</li>
+                <li className="px-2 py-2 text-center flex-1 font-extrabold">Action</li>
             </ul>
             {product.length > 0 ? (
                 product.map((item, index) => (
@@ -80,13 +80,13 @@ const Productlist = () => {
                         key={index}
                         className="flex justify-around items-center p-2 border border-blue-400 bg-blue-100 rounded-xl shadow-sm text-sm font-medium text-gray-700 tracking-wide w-full max-w-full mx-auto my-1"
                     >
-                        <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">{index + 1}</li>
+                        <li className="px-2 py-2 text-center flex-1 border-r border-gray-300 font-bold">{index + 1}</li>
                         <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">{item.name}</li>
                         <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">${item.price}</li>
                         <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">{item.category}</li>
                         <li className="px-2 py-2 text-center flex-1 border-r border-gray-300">{item.company}</li>
                         <li className="px-2 py-2 text-center flex-1">
-                            <button onClick={() => deleteProduct(item._id)}>Delete</button>
+                            <button className='underline hover:cursor-pointer' onClick={() => deleteProduct(item._id)}>Delete</button>
                             <Link to={`/update/${item._id}`}> Update</Link>
                         </li>
                     </ul>
