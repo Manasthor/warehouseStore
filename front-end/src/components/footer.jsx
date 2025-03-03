a76d708aa3d039940dc32845b20a05cf6dccbdc5
unknown <manas842500@gmail.com>
2025-03-03 12:54:07 +0530

    CSS

--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='absolute bottom-0 w-full bg-blue-200 text-center p-4'>
+        <div className='bottom-0 w-full bg-blue-200 text-center p-4'>
             <h3>E-Comm Dashboard</h3>
         </div>
     )
