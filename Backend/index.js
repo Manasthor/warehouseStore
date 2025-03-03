287ec9a97c9dfed2356c925512bb59a1360eeac6
unknown <manas842500@gmail.com>
2025-03-03 11:27:14 +0530

    JWT

--- Backend/index.js
+++ Backend/index.js
@@ -128,7 +128,7 @@ function verifyToken(req, res, next) {
     }
 }
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
